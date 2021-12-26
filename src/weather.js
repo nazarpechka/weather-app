b9efec712dd822e4b88324425d203db9a9a2a93b
Nazar Pechevystyi <nazarpechka@gmail.com>
2021-12-26 21:52:25 +0100

    Return weather data in readable format

--- src/weather.js
+++ src/weather.js
@@ -1,11 +1,24 @@
 const apiKey = 'cfc186d360b6183d2efa05df0eb38497';
 
-async function weatherData(city = 'Kyiv') {
+async function requestWeather(city, units) {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=${units}`
   );
   const data = await response.json();
   return data;
+}
+
+async function weatherData(city = 'Kyiv', units = 'metric') {
+  const weather = await requestWeather(city, units);
+  return {
+    city: weather.name,
+    country: weather.sys.country,
+    temp: weather.main.temp,
+    humidity: weather.main.humidity,
+    windSpeed: weather.wind.speed,
+    weatherId: weather.weather[0].id,
+    weatherName: weather.weather[0].main,
+  };
 }
 
 export default weatherData;
