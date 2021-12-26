38fb373663b47fb7da4517f62532f73f9d8eb4f1
Nazar Pechevystyi <nazarpechka@gmail.com>
2021-12-26 20:42:22 +0100

    Don't use polyfills, support for Chrome

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+            presets: [['@babel/preset-env', { targets: 'last 2 Chrome versions' }]],
           },
         },
       },
